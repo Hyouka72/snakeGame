e15dba42cd49c6703fe622d5b2d8b8fde3f5c4dd
Hyouka45 <khewang212@gmail.com>
2025-11-17 21:13:55 +0545

    snake game comeleted

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -12,7 +12,7 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
-    static final int DELAY = 75;
+    static int DELAY = 100;
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
     int bodyparts = 6;
@@ -63,7 +63,14 @@ public class GamePanel extends JPanel implements ActionListener {
                     g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                 }
             }
+
+            g.setColor(Color.RED);
+            g.setFont(new Font("Ink Free",Font.BOLD, 75));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Source: "+applesEaten))/2, g.getFont().getSize());
+
         }
+
         else {
             gameOver(g);
         }
@@ -98,6 +105,10 @@ public class GamePanel extends JPanel implements ActionListener {
         if( x[0] == appleX && y[0] == appleY){
             bodyparts++;
             applesEaten++;
+            if(DELAY > 50){
+                DELAY -= 1;
+                timer.setDelay(DELAY);
+            }
             newApple();
         }
     }
@@ -127,6 +138,11 @@ public class GamePanel extends JPanel implements ActionListener {
         g.setFont(new Font("Ink Free",Font.BOLD, 75));
         FontMetrics metrics = getFontMetrics(g.getFont());
         g.drawString("GAME OVER", (SCREEN_WIDTH - metrics.stringWidth("GAME OVER"))/2, SCREEN_HEIGHT/2);
+
+        g.setColor(Color.RED);
+        g.setFont(new Font("Ink Free",Font.BOLD, 50));
+        FontMetrics metrics1 = getFontMetrics(g.getFont());
+        g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics1.stringWidth("Source: "+applesEaten))/2, g.getFont().getSize());
     }
     @Override
     public void actionPerformed(ActionEvent e) {
